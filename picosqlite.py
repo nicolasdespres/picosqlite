f11900df7d8a805fa1f61a3412343a3d6b1f22de
Nicolas Despres <nicolas.despres@gmail.com>
2023-02-04 20:56:46 +0100

    Explicitly bind the accelerator on all platform.
    With Python 3.10.8 on macOS, I experienced the same issue than on
    Windows. Explicitly binding the accelerator, solves the issue and
    the shortcut works again.

--- picosqlite.py
+++ picosqlite.py
@@ -1310,14 +1310,13 @@ class Application(tk.Frame):
         self.help_menu.add_separator()
         self.help_menu.add_command(label="About...", command=self.about_action)
 
-        # On Windows, we have to explicitly bind the accelerator...
-        if running_on_windows():
-            self.master.bind_all("<F2>", lambda _: self.open_action())
-            self.master.bind_all("<F3>", lambda _: self.run_query_action())
-            self.master.bind_all("<F5>", lambda _: self.refresh_action())
-            self.master.bind_all("<F7>", lambda _: self.clear_result_action())
-            self.master.bind_all("<F9>", lambda _: self.delete_rows_action())
-            self.master.bind_all("<F12>", lambda _: self.interrupt_action())
+        # Explicitly bind the accelerator...
+        self.master.bind_all("<F2>", lambda _: self.open_action())
+        self.master.bind_all("<F3>", lambda _: self.run_query_action())
+        self.master.bind_all("<F5>", lambda _: self.refresh_action())
+        self.master.bind_all("<F7>", lambda _: self.clear_result_action())
+        self.master.bind_all("<F9>", lambda _: self.delete_rows_action())
+        self.master.bind_all("<F12>", lambda _: self.interrupt_action())
 
     def init_layout(self):
         # Doc: https://tkdocs.com/tutorial/grid.html#resize
